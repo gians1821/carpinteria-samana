cc35c9ef6a2e5bff515ce1ba0fc6cf3c82ace2b4
gians1821 <gian.samana.1801@gmail.com>
2024-04-12 11:48:49 -0500

    Updates

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+const flagsElement = document.querySelector("#flags")
+
+const changeLanguage = async language => {
+    const requestJson = await fetch(`./lang/${language}.json`)
+    const texts = await requestJson.json()
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section
+        const value = textToChange.dataset.value
+
+        textToChange.innerHTML = texts[section][value]
+    }
+}
+
+const textsToChange = document.querySelectorAll('[data-section]')
+
 const toggleMenu = () => {
     document.querySelector("#menu").classList.toggle('right-0')
     document.querySelector("#menu").classList.toggle('-right-full')
@@ -7,6 +23,11 @@ const toggleDark = () => {
     document.querySelector("html").classList.toggle('dark')
 }
 
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.parentElement.dataset.language)
+})
+
+/*
 const toPEN = () => {
     location.href = "../../index.html"
 }
@@ -14,3 +35,4 @@ const toPEN = () => {
 const toUSA = () => {
     location.href = "./src/en/index.html"
 }
+*/
